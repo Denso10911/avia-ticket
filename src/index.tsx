fb0ac426ed4f0b78e7b18593b30a38501b501726
denso10911 <denso10911@gmail.com>
2024-05-12 14:32:38 +0300

    Add basename to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root")!)
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={"/avia-ticket"}>
       <App />
     </BrowserRouter>
   </Provider>,
